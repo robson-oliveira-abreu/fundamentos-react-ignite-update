674a5509c2920179637f0916f6ee428b6b2efbff
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-06-03 11:35:10 -0300

    first commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+import styles from './Post.module.css'
+
+export function Post(){
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.}></div>
+            </header>
+        </article>
+    )
+}
