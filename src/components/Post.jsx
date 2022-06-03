1d4b71fd498e026423d8522da498443b9716e7e9
robson <robsonsilva.abreu@outlook.com>
2022-06-03 19:45:26 -0300

    update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,39 @@
 import styles from './Post.module.css'
 
-export function Post(){
+export function Post() {
     return (
         <article className={styles.post}>
             <header>
-                <div className={styles.}></div>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src='https://github.com/robson-oliveira-abreu.png' />
+                    <div className={styles.authorInfo}>
+                        <strong>Robson Oliveira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title='03 de junho ás 10:49' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
             </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉{' '}<a href='#'>jane.design/doctorcare</a></p>
+                <p>
+                    <a href='#'>#novoprojeto</a>{' '}
+                    <a href='#'>#nlw</a>{' '}
+                    <a href='#'>#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+
+                <button type="submit">comentário</button>
+            </form>
         </article>
     )
 }
